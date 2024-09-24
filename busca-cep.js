61e5bf794de53c92e25801197e90e82e6fcf9506
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2024-09-24 20:39:20 -0300

    inclui formulário de endereço e função para buscar os dados no viacep

--- busca-cep.js
+++ busca-cep.js
@@ -1,5 +1,31 @@
+const URL_BASE_VIACEP = "https://viacep.com.br/ws";
+
+function preencheForm(dados) {
+  console.log(dados);
+}
+
 function buscaCep() {
-  console.log("Clicou no botão!");
+  const inputCep = document.getElementById("input-cep");
+
+  const cep = inputCep.value;
+
+  const url = `${URL_BASE_VIACEP}/${cep}/json/`;
+
+  fetch(url)
+    .then((response) => {
+      if (response.status !== 200) {
+        throw new Error("Falha na requisição do cep");
+      }
+      return response.json();
+    })
+    .then((dados) => {
+      if ("erro" in dados && dados.erro === "true") {
+        throw new Error("CEP não encontrado");
+      }
+      return dados;
+    })
+    .then(preencheForm)
+    .catch((erro) => console.log(erro));
 }
 
 btn = document.getElementById("btn-buscar");
