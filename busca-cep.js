6069a678612a3ee2600825b23e8fa112ebe80e4f
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2024-09-26 22:09:01 -0300

    adiciona preenchimento automatico ao buscar o cep

--- busca-cep.js
+++ busca-cep.js
@@ -1,5 +1,60 @@
-function buscaCep(evento) {
-  console.log(evento);
+const BASE_URL_VIACEP = "https://viacep.com.br/ws";
+
+// bairro: "Bom Retiro"
+// estado: "São Paulo"
+// localidade: "São Paulo"
+// logradouro: "Rua Ribeiro de Lima"
+
+function preencheInput(id, valor) {
+  const input = document.getElementById(id);
+  input.value = valor;
+  input.readOnly = true;
+}
+
+function preencheCep(dados) {
+  preencheInput("rua", dados.logradouro);
+  preencheInput("bairro", dados.bairro);
+  preencheInput("cidade", dados.localidade);
+  preencheInput("estado", dados.uf);
+
+  const inputNumero = document.getElementById("numero");
+  inputNumero.focus();
+}
+
+function buscaCep() {
+  inputCep = document.getElementById("input-cep");
+
+  cep = inputCep.value.replaceAll(/\D/g, "");
+
+  console.log("ciclou no botão", cep);
+
+  if (cep.length !== 8) {
+    alert("CEP inválido, verifique o valor digitado");
+    return null;
+  }
+
+  inputCep.value = cep;
+
+  const url = `${BASE_URL_VIACEP}/${cep}/json/`;
+
+  fetch(url)
+    .then((response) => {
+      if (response.status !== 200) {
+        throw new Error("Erro ao consultar a api do viacep");
+      }
+      return response.json();
+    })
+    .then((dados) => {
+      if ("erro" in dados && dados.erro === "true") {
+        throw new Error("CEP não encontrado");
+      }
+      return dados;
+    })
+    .then(preencheCep)
+    .catch((e) => {
+      alert("Erro ao consultar o cep");
+      console.log(e);
+    });
 }
 
 btn = document.getElementById("btn-cep");
