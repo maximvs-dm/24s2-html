a0118e074013bac1cd890c95e6e79c4fd2d4d166
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2024-10-08 20:19:54 -0300

    adiciona exemplo de criação de cards com a PokeAPI

---
+++ pokeapi/scripts.js
@@ -0,0 +1,74 @@
+function criaCard(dados) {
+  const containerPai = document.getElementById("container");
+
+  const card = document.createElement("div");
+  card.style.border = "solid 1px black";
+  card.style.borderRadius = "8px";
+  card.style.textAlign = "center";
+  card.style.padding = "4px";
+  card.style.margin = "4px";
+  card.style.width = "fit-content";
+
+  const foto = document.createElement("img");
+  const nome = document.createElement("p");
+  const vida = document.createElement("p");
+
+  foto.src = dados.imageSrc;
+  nome.textContent = dados.nome;
+  vida.textContent = dados.hp;
+
+  card.appendChild(foto); //sprites.front_default
+  card.appendChild(nome); //name
+  card.appendChild(vida);
+
+  containerPai.appendChild(card);
+}
+
+// {
+//   base_stat: 35,
+//   stat: {
+//     name: "hp"
+//   }
+// }
+
+function buscaPokemon() {
+  const input = document.getElementById("nome-pokemon");
+  const nome = input.value;
+
+  console.log("nome digitado:", nome);
+
+  if (!nome) {
+    alert("Digite o nome do Pokemon");
+    return null;
+  }
+
+  const url = `https://pokeapi.co/api/v2/pokemon/${nome}`;
+
+  fetch(url)
+    .then((resposta) => {
+      if (resposta.status === 404) {
+        throw new Error("Pokemon não encontrado");
+      }
+      if (resposta.status !== 200) {
+        throw new Error("Erro desconhecido");
+      }
+      return resposta.json();
+    })
+    .then((dados) => {
+      const hpStat = dados.stats.find((f) => f.stat.name === "hp");
+      console.log(hpStat);
+
+      const dadosUteis = {
+        imageSrc: dados.sprites.front_default,
+        nome: dados.name,
+        hp: hpStat.base_stat,
+      };
+
+      console.log(dadosUteis);
+      criaCard(dadosUteis);
+    })
+    .catch((erro) => alert(erro.message));
+}
+
+const btnBuscar = document.getElementById("buscar-pokemon");
+btnBuscar.addEventListener("click", buscaPokemon);
